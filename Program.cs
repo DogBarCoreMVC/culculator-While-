6daa7ae654161237f3d9b89cb8289339a9930c5b
GitDogBar <Dogbar>
2021-08-10 14:33:40 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace culculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "yes";
+            while (choice == "yes")
+            {
+                Console.Write("Input rount:");
+                int rount = (int)double.Parse(Console.ReadLine());
+                Console.WriteLine("\n");
+                int result = 0;
+
+                while (rount > 0)
+                {
+                    Console.Write("Input Number:");
+                    int number = (int)double.Parse(Console.ReadLine());
+                    result += number;
+                    rount--;
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("Result = " + result);
+
+                Console.Write("Want to be active? (yes/on):");
+                choice = Console.ReadLine();
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Goog Bye");
+        }
+    }
+}
